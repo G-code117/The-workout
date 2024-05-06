0ceedbb2a9dc604a483b3ed32169b51bdcfa7bee
jasongalas <jasongalas@gmail.com>
2024-05-06 10:42:51 -0700

    Updated apicall to allow for multiple keys to be used, fixed file name of connections..js

--- utils/apicall.js
+++ utils/apicall.js
@@ -1,12 +1,29 @@
+require('dotenv').config();
 const axios = require('axios');
 
+// Define an array of API keys through the env
+const apiKeys = [
+  process.env.API_KEY_1,
+  process.env.API_KEY_2,
+  process.env.API_KEY_3,
+  process.env.API_KEY_4
+];
+
+const getRandomApiKey = () => {
+  // Randomly select an API key from the array above
+  const randomIndex = Math.floor(Math.random() * apiKeys.length);
+  return apiKeys[randomIndex];
+};
+
+//API fetch call
 const fetchData = async () => {
+  const apiKey = getRandomApiKey();
     const options = {
         method: 'GET',
         url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/back',
         params: {limit: '10'},
         headers: {
-          'X-RapidAPI-Key': '65d425a615msh8b10438e995f535p1d297ajsn78f72a232e93',
+          'X-RapidAPI-Key': apiKey,
           'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
         }
       };
