b50876b3377f8a43cdd101045836ec6b26a1f2e3
jasongalas <jasongalas@gmail.com>
2024-05-08 14:19:07 -0700

    Added our NPM package function to the login logic

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,5 @@
+const calculate = require('fitness-health-calculations');
+
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
@@ -20,17 +22,27 @@ const loginFormHandler = async (event) => {
     }
   };
   
+  function BMR(gender, age, height, weight) {
+    let myBmr = calculate.bmr(gender, age, height, weight);
+    return myBmr;
+} 
+
   const signupFormHandler = async (event) => {
     event.preventDefault();
     
     const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+    const gender = document.querySelector("#gender").value;
+    const age = parseInt(document.querySelector("#age").value);
+    const height = parseFloat(document.querySelector("#height").value);
+    const weight = parseFloat(document.querySelector("#weight").value);
   
-    if (name && email && password) {
+    if (name && email && password && !isNaN(age) && !isNaN(height) && !isNaN(weight)) {
+      const myBmr = BMR(gender, age, height, weight);
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ name, email, password, gender, age, height, weight }),
         headers: { 'Content-Type': 'application/json' },
       });
   
