bfdb7576015dea2e2986020a6bd97d92d78746af
jasongalas <jasongalas@gmail.com>
2024-05-08 20:51:52 -0700

    Exercise route added to api index js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const exerciseRoutes = require('./exercise-routes');
+const workoutRoutes = require('./workout-routes')
 
 router.use('/user', userRoutes);
 router.use('/exercise', exerciseRoutes);
+router.use('/workout', workoutRoutes);
 
 module.exports = router;
