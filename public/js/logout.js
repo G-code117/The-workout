87ac64fa0b474a0bc6e559a7450bfffef0813f38
Commissioner Gordon <geden11@gmail.com>
2024-05-09 14:48:34 -0700

    fixed seed

--- public/js/logout.js
+++ public/js/logout.js
@@ -7,7 +7,7 @@ const logout = async () => {
   
     if (response.ok) {
 
-      document.location.replace('/login');
+      document.location.replace('/');
     } else {
       alert('Failed to log out');
     }
--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,11 @@
 const sequelize = require('../config/connection');
-const { User, MuscleGroup, Exercise } = require('../models');
+const { User, MuscleGroup, Exercise, Workout, Routine } = require('../models');
 
 const seedUsers = require('./user.json');
 const seedExercise = require('./exerciseData.json');
 const seedMuscleGroup = require('./muscleGroupData.json');
+const seedWorkout = require('./workoutData.json');
+const seedRoutine = require('./routineData.json');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
@@ -13,16 +15,15 @@ const seedAll = async () => {
       returning: true,
     });
   
-    await MuscleGroup.bulkCreate(seedMuscleGroup, {
-      individualHooks: true,
-      returning: true,
-    });
+    await MuscleGroup.bulkCreate(seedMuscleGroup);
 
-    await Exercise.bulkCreate(seedExercise, {
-      individualHooks: true,
-      returning: true,
-    });
+    await Exercise.bulkCreate(seedExercise);
   
+    await Workout.bulkCreate(seedWorkout);
+
+    await Routine.bulkCreate(seedRoutine);
+
+    console.log('seed complete')
     process.exit(0);
   };
   
