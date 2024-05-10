7291dff4ed8382e90467ff595ba57a12726d4c8b
ZachariahKB <zachariahakb@gmail.com>
2024-05-09 20:10:36 -0700

    yo

--- public/js/user.js
+++ public/js/user.js
@@ -1,20 +1,24 @@
-function getWorkoutsByMuscleGroup (groupName) {
- 
-}
+// function displayWorkouts(workouts) {
 
-function displayWorkouts(workouts) {
+//     const workoutContainer = ('#workout-container');
+//     workoutContainer.empty();
 
-    const workoutContainer = ('#workout-container');
-    workoutContainer.empty();
+//     workouts.forEach(function(workout){
+//         let $workoutElement = $('<div>', { class: 'workout-item' });
+//         $workoutElement.append($('<h4>', { text: workout.name }));
+//         $workoutElement.append($('<img>', { src: workout.gif, alt: workout.name }));
+//         $workoutElement.append($('<p>', { text: workout.equipment }));
+//         $workoutElement.append($('<p>', { text: workout.instructions }));
 
-    workouts.forEach(function(workout){
-        let $workoutElement = $('<div>', { class: 'workout-item' });
-        $workoutElement.append($('<h4>', { text: workout.name }));
-        $workoutElement.append($('<img>', { src: workout.gif, alt: workout.name }));
-        $workoutElement.append($('<p>', { text: workout.equipment }));
-        $workoutElement.append($('<p>', { text: workout.instructions }));
-
-        // Append workout element to container
-        $workoutContainer.append($workoutElement);
-    });
-}
+//         // Append workout element to container
+//         $workoutContainer.append($workoutElement);
+//     });
+// }
+const workouts = document.querySelector("#workouts");
+console.log(workouts);
+const currentWorkout = async (event) => {
+  event.preventDefault();
+  const id = event.target.id;
+  document.redirect(`/workout/${id}`);
+};
+workouts.addEventListener("click", currentWorkout);
