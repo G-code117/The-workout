16542c531ef7e864c013bb80668543967dcbaf31
Jensen <163614828+LockedJCE@users.noreply.github.com>
2024-05-09 18:55:30 -0700

    partial fix

--- public/js/login.js
+++ public/js/login.js
@@ -13,7 +13,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/user');
+        document.location.replace('/');
       } else {
         alert('OH NO!',response.statusText);
       }
